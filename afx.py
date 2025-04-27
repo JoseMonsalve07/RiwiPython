2c64ea989d3b157ae3345f21467598c2e222d49d
Jose Monsalve <00jsmonsalve@gmail.com>
2025-04-27 18:52:49 -0500

    FuncYReturn

---
+++ afx.py
@@ -0,0 +1,11 @@
+def multiplicar (a, b):
+    resultado = a * b
+    return resultado
+ 
+name="a"
+productValue = multiplicar(2, 8)
+
+resultado=productValue + 4
+
+print(productValue)
+print(resultado)
