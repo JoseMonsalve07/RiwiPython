e0002e5c6883b8e50beb23136dbe7f8b4d00a0d6
Jose Monsalve <00jsmonsalve@gmail.com>
2025-04-29 09:57:48 -0500

    EjmsFor

---
+++ EstMood.py
@@ -0,0 +1,14 @@
+frutas = ["Pera", "Manzana", "Guayaba"]
+
+for fruta in frutas:
+    print(fruta)
+
+ciudades = ["madrid", "barcelona", "valencia"]
+
+for ciudad in ciudades:
+    print(ciudad)
+
+mensaje = "Hola"
+
+for caracter in mensaje:
+    print(caracter)
