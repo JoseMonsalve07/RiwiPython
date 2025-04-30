f85cb7c109e95ba37784b0112b20fcb1b793f799
Jose Monsalve <00jsmonsalve@gmail.com>
2025-04-30 10:10:22 -0500

    Calificaciones

--- Calificaciones.py
+++ Calificaciones.py
@@ -9,27 +9,32 @@
 grade=float(input("Ingrese una calificación numérica de 0 a 100 "))
 while grade < 0 or grade > 100:
     grade=int(input("Ingrese un valor válido. Solo se aceptan valores entre 0 y 100 "))
-if grade >= 60:
+if grade >= 70:
     print("Usted ha aprobado")
 else:
     print("Usted ha reprobado") 
 
 grades=input("Ingrese sus calificaciones separadas por comas ',': ").split(",")
 floatGrades= [float(i) for i in grades]
-
 for grade in floatGrades:
-    add=sum(floatGrades)
-    avg=add/len(floatGrades)
-print(f"El promedio de las calificaciones en la lista es: {avg}")
+    Addition=sum(floatGrades)
+    Average=Addition/len(floatGrades)
+print(f"El promedio de las calificaciones en la lista es: {Average}")
 
 specificValue=float(input("Ingrese un valor para contar cuántas calificaciones en la lista son mayores que este valor: "))
-for comparedGrade in floatGrades:  
-    x=0
-    while specificValue==comparedGrade:
-        x+=1
+gradeGreatherThanTheValue=0
+noteCounter=0
+while noteCounter<len(floatGrades):
+    if specificValue<floatGrades[noteCounter]:
+        gradeGreatherThanTheValue+=1
+    noteCounter+=1 
+print(f"Total de calificaciones mayores al valor indicado: {gradeGreatherThanTheValue}")
 
-print(f"aaa{x}")
-
-print(grade)
-print(floatGrades)
-print(grades)
+specificGrade=float(input("Ingrese una nota para verificar si está en sus notas y contar cuántas veces está: "))
+repeatedGradeCounter=0
+for isTheGrade in floatGrades:
+    if specificGrade==isTheGrade:
+        repeatedGradeCounter+=1
+    else:
+        continue    
+print(f"Total de calificaciones con este mismo valor: {repeatedGradeCounter}")
