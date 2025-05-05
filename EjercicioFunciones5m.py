b8e104e1e7ed80baa2f8f9c7210ed8482a8ac345
Jose Monsalve <00jsmonsalve@gmail.com>
2025-05-05 10:12:14 -0500

    Ejercicio Dificil Funciones

---
+++ EjercicioFunciones5m.py
@@ -0,0 +1,28 @@
+print("Bienvenido!")
+numberOfUsers=int(input("Cantidad de usuarios a ingresar: "))
+
+
+def nameAndGrades():
+    name=input("Nombre del usuario: ")
+    userGrades=input("Ingrese tres notas (Rango de 0 a 5), separadas por comas: ").split(",")
+    grades=[float(i) for i in userGrades]
+    return name, grades
+    
+def average(grades):
+    return sum(grades)/3
+
+def results(name, averages):
+    print(f"Estudiante {name}, su promedio es {averages}")
+    if averages>=3.0:
+        print("Usted aprobó")
+    else:
+        print("Usted reprobó")
+
+usersCount=0
+for users in range(numberOfUsers):
+    while usersCount<numberOfUsers:
+        usersCount+=1
+        name, grades=nameAndGrades()
+        averages=average(grades)
+        results(name, averages)
+        
