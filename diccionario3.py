4fdf78dab497afe965ab5bed05088f306ee3306e
Jose Monsalve <00jsmonsalve@gmail.com>
2025-05-07 08:15:33 -0500

    DICCIONARIOS

--- diccionario3.py
+++ diccionario3.py
@@ -28,18 +28,27 @@ newUser={
     "telefono": "3014349376"
 }
 
+#agregar dato a la lista
 users.append(newUser)
 
+#imprimir sobre usuario especifico
 for user in users:
     if user["cc"]=="345":
         print(f"Datos: Nombre: {user["nombre"]}, CC: {user["cc"]}")
         break
-
+#cambiar dato de un usuario
 for user in users:
     if user["cc"]=="678":
         user["edad"]=35
         print(user["edad"])
         break
-    
+
+#eliminar usuario
+for user in users:
+    if user["nombre"]=="Jose Manuel":
+        users.remove(user)
+        print(users)
+        break
+
 #print(users)
 #print(len(users))
